d586ce15baa9b5c73eed24945b51a2b63de51d80
michael lim <michael.limnh@gmail.com>
2020-02-24 17:30:46 +0000

    added controller methods

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,4 +1,28 @@
 class RentalsController < ApplicationController
-  #  :new, :create
-  
+  # before_action :find_gear
+
+  # def new
+  #   @rental = Rental.new
+  # end
+
+  # def create
+  #   @rental = Rental.new(rental_params)
+  #   @rental.gear = @gear
+  #   if @rental.save
+  #     redirect_to gear_path(@gear)
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  # private
+
+  # def find_gear
+  #   @gear = Gear.find(params[:gear_id])
+  # end
+
+  # def rental_params
+  #   params.require(:rental).permit(:starts_at, :ends_at, :total_price, :status)
+  # end
+
 end
