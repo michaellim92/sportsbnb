7c616f745124bcd1ea26b76af5b7d54a0852f677
michael lim <michael.limnh@gmail.com>
2020-02-25 10:54:12 +0000

    changed :new in rentals routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :equipments do
-    resources :rentals, only: [:only, :create]
+    resources :rentals, only: [:new, :create]
   end
   resources :rentals, only: :destroy
 end
