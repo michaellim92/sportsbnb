3fba23ae247aa482241f75b60f410e6f85201579
michael lim <michael.limnh@gmail.com>
2020-02-25 20:12:34 +0000

    added rental index page

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,28 +1,27 @@
 class RentalsController < ApplicationController
-  # before_action :find_gear
+  before_action :find_gear
 
-  # def new
-  #   @rental = Rental.new
-  # end
+  def new
+    @rental = Rental.new
+  end
 
-  # def create
-  #   @rental = Rental.new(rental_params)
-  #   @rental.gear = @gear
-  #   if @rental.save
-  #     redirect_to gear_path(@gear)
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    @rental = Rental.new(rental_params)
+    @rental.gear = @gear
+    if @rental.save
+      redirect_to gear_path(@gear)
+    else
+      render :new
+    end
+  end
 
-  # private
+  private
 
-  # def find_gear
-  #   @gear = Gear.find(params[:gear_id])
-  # end
+  def find_gear
+    @gear = Gear.find(params[:gear_id])
+  end
 
-  # def rental_params
-  #   params.require(:rental).permit(:starts_at, :ends_at, :total_price, :status)
-  # end
-
+  def rental_params
+    params.require(:rental).permit(:starts_at, :ends_at, :total_price, :status)
+  end
 end
