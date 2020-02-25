88e498b88ba6e45f57b1328a75e7f63450446f98
michael lim <michael.limnh@gmail.com>
2020-02-25 11:17:44 +0000

    added sign in authenticated and unauthenticated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  
+  unauthenticated do
+    devise_scope :user do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+ 
+ authenticated :user do
+  root to: 'gears#dashboard'
+ end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :equipments do
-    resources :rentals, only: [:only, :create]
+  resources :gears do
+    resources :rentals, only: [:new, :create]
   end
   resources :rentals, only: :destroy
 end
